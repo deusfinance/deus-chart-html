eb3d2cd30a7dbbc54a528451860ae800d7da75c5
MMD <sirmmds@gmail.com>
2020-11-29 13:17:10 +0330

    update resolutions

--- src/helpers.js
+++ src/helpers.js
@@ -12,54 +12,65 @@ const self = module.exports = {
      * @output {url} String
      */
     buildUrlFromQuery: (query) => {
-        return (query.type == "Stock")
-            ? self.buildFinnhubURL(query)
-            : self.buildDatabaseURL(query);
+        return (query.type == "Stock") ?
+            self.buildFinnhubURL(query) :
+            self.buildDatabaseURL(query);
     },
 
-    buildFinnhubURL: ({ symbol, resolution, from, to } = query) => {
+    buildFinnhubURL: ({
+        symbol,
+        resolution,
+        from,
+        to
+    } = query) => {
 
         // Finnhub API accepts the fields, whilst Tradingview works with the keys.
         const interval = {
-        		"1": "1",
-        		"1m": "1",
-        		"5": "5",
-        		"5m": "5",
-        		"15": "15",
-        		"15m": "15",
-        		"30": "30",
-        		"30m": "30",
-        		"60": "60",
-        		"60m": "60",
-        		"D": "D",
-        		"1D": "D",
-        		"W": "W",
-        		"1W": "W",
-        		"M": "M",
-        		"1M": "M"
-        }[resolution]
+            "1": "1",
+            "1m": "1",
+            "5": "5",
+            "5m": "5",
+            "15": "15",
+            "15m": "15",
+            "30": "30",
+            "30m": "30",
+            "60": "60",
+            "60m": "60",
+            "D": "D",
+            "1D": "D",
+            "W": "W",
+            "1W": "W",
+            "M": "M",
+            "1M": "M"
+        } [resolution]
         return `https://finnhub.io/api/v1/stock/candle?symbol=${symbol}&resolution=${interval}&from=${from}&to=${to}&token=${process.env.FINNHUB_API_KEY}`
     },
 
-    buildDatabaseURL: ({ symbol, pair, resolution, from, to } = query) => {
+    buildDatabaseURL: ({
+        symbol,
+        pair,
+        resolution,
+        from,
+        to
+    } = query) => {
         const interval = {
-            "1": "1m",
-            "1m": "1m",
-            "5": "5m",
-            "5m": "5m",
-            "15": "15m",
-            "15m": "15m",
-            "30": "30m",
-            "30m": "30m",
-            "60": "1h",
-            "60m": "1h",
-            "D": "1d",
-            "1D": "1d",
-            "W": "1W",
-            "1W": "1W",
-            "M": "1M",
-            "1M": "1M"
-        }[resolution]
+            "1": "1",
+            "1m": "1",
+            "5": "5",
+            "5m": "5",
+            "15": "15",
+            "15m": "15",
+            "30": "30",
+            "30m": "30",
+            "60": "60",
+            "60m": "60",
+            "D": "1440",
+            "1D": "1440",
+            "W": "10080",
+            "1W": "10080",
+            "M": "43200",
+            "1M": "43200"
+        } [resolution]
 
         // return `http://127.0.0.1:3000/candles?symbol=${symbol + pair}&interval=${interval}&from=${from}&to=${to}`
         return `${process.env.DEUS_CANDLES_URL}/candles?symbol=${symbol + pair}&interval=${interval}&from=${from}&to=${to}`
