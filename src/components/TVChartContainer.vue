9fedf04e1830e2baa88a8456fc338a537d6a7251
MMD <sirmmds@gmail.com>
2021-02-01 13:02:22 +0330

    add iframe mode

--- src/components/TVChartContainer.vue
+++ src/components/TVChartContainer.vue
@@ -22,26 +22,59 @@ export default {
     };
   },
   mounted() {
-    const widgetOptions = {
+    let widgetOptions = {
       symbol: "deus/eth",
       // BEWARE: no trailing slash is expected in feed URL
       datafeed: new window.Datafeeds.UDFCompatibleDatafeed(
         process.env.VUE_APP_DATAFEED_URL
       ),
-      interval: "60m",
+      interval: "1D",
       container_id: "tv_chart_container",
       library_path: "/charting_library/",
 
       locale: getLanguageFromURL() || "en",
-      disabled_features: ["use_localstorage_for_settings"],
-      enabled_features: ["study_templates"],
       charts_storage_api_version: "1.1",
       client_id: "DEUS",
       user_id: "public_user_id",
       fullscreen: true,
       autosize: true,
       theme: "Dark",
+
+      exchanges: [
+        {
+          value: "DEUS Swap",
+          name: "DEUS Swap",
+          desc: "DEUS Swap",
+        },
+      ],
+      symbols_types: [
+        {
+          name: "crypto",
+          value: "crypto",
+        },
+      ],
+
+      interval: "1D",
+      supported_resolutions: ["60", "D"],
+
+      supports_marks: false,
+      supports_timescale_marks: false,
+      supports_time: true,
+
+      supports_search: true,
+      supports_group_request: false,
     };
+
+    let uri = window.location.search.substring(1);
+    let params = new URLSearchParams(uri);
+    let isIframe = params.get("iframe") != undefined;
+    if (isIframe) {
+      widgetOptions.disabled_features = [
+        "header_symbol_search",
+        "left_toolbar",
+        "header_indicators",
+      ];
+    }
 
     const tvWidget = new widget(widgetOptions);
     this.tvWidget = tvWidget;
